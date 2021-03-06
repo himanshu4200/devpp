2cfb3e32b7ccca1beb134f847b7c460e0e3fab4b
himanshu <himanshupandey9123@gmail.com>
2021-03-06 19:10:26 +0530

    commit

--- LEC0/intro.js
+++ LEC0/intro.js
@@ -1,1 +1,21 @@
 console.log("hello world");
+let data = {
+    name: "Steve Rogers",
+    place: "Queens",
+    values: [
+      10,
+      false,
+      {
+        name: "Steve Rogers",
+        place: "Queens",
+      },
+      "Hey i am a value",
+      [1, 2, 3, 4, 5, 6],
+    ],
+    movies : {
+        name:"Captain America",
+        rating:10
+    }
+  };
+  console.log(data.name);
+  
